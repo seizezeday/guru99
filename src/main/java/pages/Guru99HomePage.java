a5fca57a4e4bc6a845cab5e55e7491c402cdd325
Vasyl Horokhovskyi <vasyl.horokhovskyi@redknee.com>
2017-07-06 00:20:29 +0200

    initial. chrome driver

---
+++ src/main/java/pages/Guru99HomePage.java
@@ -0,0 +1,7 @@
+package pages;
+
+/**
+ * Created by vhorokho on 2017-07-05.
+ */
+public class Guru99HomePage {
+}
---
+++ src/test/java/TestGuru99Login.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by vhorokho on 2017-07-05.
+ */
+public class TestGuru99Login {
+}
